1a96ccc348eb71b65b11ab27349f921483e63c3c
Jatin <jatin.kshatriya2821@gmail.com>
2024-09-18 22:23:11 +0530

    production conifg

--- src/handlers/apiHandler.js
+++ src/handlers/apiHandler.js
@@ -2,7 +2,7 @@ import { logout } from "@/lib/auth/session";
 import { redirect } from "next/navigation";
 
 // Base URL of the API, use environment variable
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
+const BASE_URL = process.env.SERVER_URL || "http://localhost:8000";
 
 // Default headers for all requests
 const defaultHeaders = {
