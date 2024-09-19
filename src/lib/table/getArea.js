a5e9e313ba1b58a69aa84feeb26d6380668fa9aa
Jatin <jatin.kshatriya2821@gmail.com>
2024-09-20 01:48:31 +0530

    table filter

--- src/lib/table/getArea.js
+++ src/lib/table/getArea.js
@@ -3,6 +3,17 @@ import { apiGet } from "@/handlers/apiHandler";
 import { notFound } from "next/navigation";
 
 export async function getArea() {
-  const response = await apiGet("/api/shop/area");
+  const user = await getSession();
+  const response = await apiGet("/api/shop/area", {
+    headers: {
+      Authorization: `Bearer ${user?.tokens?.access}`,
+      cache: "no-store",
+    },
+  });
+  if (response.status === 404) return notFound();
+  if (response.status === 401) {
+    logout();
+    redirect("/login");
+  }
   return response;
 }
